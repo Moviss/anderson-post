ad5eb2578802ab2515a156fb1f436160109239e4
Marcin Lubowicz <marcin.lubowicz@gmail.com>
2021-12-27 16:57:22 +0100

    add loading and error handling

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -5,31 +5,43 @@ import { Article } from '../types/News';
 import { CategoriesList } from '../types/Categories';
 
 interface NewsListProps {
-  currentCategory: "" | CategoriesList
+  currentCategory: '' | CategoriesList;
 }
 
 const APIKEY = '9abf605c94544df181133c876281ec23';
 
-const NewsList = ({currentCategory}: NewsListProps) => {
+const NewsList = ({ currentCategory }: NewsListProps) => {
   const [newsData, setNewsData] = useState<Array<Article> | []>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   const fetchNews = async () => {
     setIsLoading(true);
     // eslint-disable-next-line no-template-curly-in-string
-    const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us${currentCategory ? '&category=' + currentCategory : ''}&pageSize=11&page=1&apiKey=${APIKEY}`);
-    setNewsData(result.data.articles);
-    setIsLoading(false);
-  }
+    await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us${currentCategory
+        ? '&category=' + currentCategory
+        : ''}&pageSize=11&page=1&apiKey=${APIKEY}`)
+    .then(result => {
+      setHasError(false);
+      setNewsData(result.data.articles);
+    })
+    .catch(err => setHasError(true))
+    .finally(() => setIsLoading(false));
+  };
 
   useEffect(() => {
     fetchNews();
   }, [currentCategory]);
 
-
   return (
     <section className={styles.newsList}>
-      {newsData.map(newsItem => (<div key={newsItem.title}>{newsItem.title}</div>))}
+      {hasError && !isLoading &&
+      <div className={styles.error}>Some error occurred.</div>}
+
+      {(!isLoading) ? newsData.map(
+        newsItem => (<div key={newsItem.title}>{newsItem.title}</div>)) : <div
+        className={styles.error}>Loading news...</div>}
     </section>
   );
 };
